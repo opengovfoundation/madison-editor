63dbeb917f9f7c82570d6196a26c979425555d8e
Bill Hunt <bill@krues8dr.com>
2015-01-03 16:48:52 -0500

    Create confirmation button before deleting.

--- assets/app/documents/documents.js
+++ assets/app/documents/documents.js
@@ -29,19 +29,44 @@ app.controller("DocumentListController", function ($scope, $http, $routeParams, 
       alert('Houston, we got a problem!');
     });
 
+  $scope.showForm = false;
+  $scope.title = 'Untitled Document';
+  $scope.showCreateForm = function(){
+    $scope.showForm = true;
+  };
+
   $scope.newDocument = '';
   $scope.createDocument = function(){
-    var doc = {document: {'title': 'Untitled'}};
+    if(!$scope.title){
+      $scope.title = 'Untitled Document';
+    }
+    var doc = {document: {'title': $scope.title}};
 
     $http.post("/api/docs/", doc)
       .success(function (data) {
-        console.log(data.document, data.document.id);
-        $location.path('/documents/' + data.document.id);
+        console.log(data.document, data.document.slug);
+        $location.path('/documents/' + data.document.slug);
       })
       .error(function (data) {
         alert('Houston, we got a problem!');
       });
   }
+
+  $scope.deleteConfirmOpen = [];
+
+  $scope.openDeleteConfirm = function(doc) {
+    $scope.deleteConfirmOpen[doc.id] = true;
+  }
+
+  $scope.closeDeleteConfirm = function(doc) {
+    $scope.deleteConfirmOpen[doc.id] = false;
+  }
+
+  $scope.deleteDocument = function(doc) {
+    // Do document deletion here.
+    $scope.closeDeleteConfirm(doc);
+  }
+
 });
 
 app.controller("DocumentDetailController", function ($scope, $http, $routeParams) {
