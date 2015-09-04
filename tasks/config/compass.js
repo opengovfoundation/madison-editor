7a4f4bd529d2fef2c6bd7089d32316d6fd40114e
Bill Hunt <bill@krues8dr.com>
2015-09-04 15:00:04 -0400

    Merge styles from main @opengovfoundation/madison theme. Also resolves #12

--- tasks/config/compass.js
+++ tasks/config/compass.js
@@ -1,25 +1,29 @@
 module.exports = function (grunt) {
-	grunt.config.set('compass', {
-		prod: {
-			options: {
-				importPath: [
-					'assets/bower_components/bootstrap-sass-official/assets/stylesheets/',
-				],
-				sassDir: 'assets/scss',
-				cssDir: 'assets/css',
-				environment: 'prod'
-			}
-		},
-		dev: {
-			options: {
-				importPath: [
-					'assets/bower_components/bootstrap-sass-official/assets/stylesheets/',
-				],
-				sassDir: 'assets/scss',
-				cssDir: 'assets/css'
-			}
-		}
-	});
+  grunt.config.set('compass', {
+    prod: {
+      options: {
+        importPath: [
+          'assets/bower_components/compass-breakpoint/stylesheets',
+          'assets/bower_components/bootstrap-sass-official/assets/stylesheets',
+          'assets/bower_components/font-awesome/scss',
+        ],
+        sassDir: 'assets/scss',
+        cssDir: 'assets/css',
+        environment: 'prod'
+      }
+    },
+    dev: {
+      options: {
+        importPath: [
+          'assets/bower_components/compass-breakpoint/stylesheets',
+          'assets/bower_components/bootstrap-sass-official/assets/stylesheets',
+          'assets/bower_components/font-awesome/scss',
+        ],
+        sassDir: 'assets/scss',
+        cssDir: 'assets/css'
+      }
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
 };
