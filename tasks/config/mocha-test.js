6a5a0965ed9a94b6e1e0ca24a43f0cc3880fa963
Chris Birk <cmbirk@gmail.com>
2015-12-02 16:15:40 -0500

    update comment header on mocha-test

--- tasks/config/mocha-test.js
+++ tasks/config/mocha-test.js
@@ -1,6 +1,14 @@
 /**
-*	Mocha *server-side* test runner
-*/
+ * Mocha **server-side** test runner
+ *
+ * ---------------------------------------------------------------
+ *
+ * Run the mocha test runner for our test suite
+ *
+ * For usage docs see:
+ * 		https://github.com/pghalliday/grunt-mocha-test
+ *
+ */
 
 module.exports = function (grunt) {
 	
